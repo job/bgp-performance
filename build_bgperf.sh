fd2bc0570656c5b8fc834a3ae5739fe79e624127
Job Snijders <job@sobornost.net>
2024-12-18 14:04:27 +0000

    add to builder

--- build_bgperf.sh
+++ build_bgperf.sh
@@ -3,6 +3,6 @@
 echo 16384 | sudo tee /proc/sys/net/ipv4/neigh/default/gc_thresh3
 sudo swapoff -a
 
-for i in openbgp87 openbgp86 bird gobgp; do
+for i in openbgp87 openbgp86 bird bird3 gobgp; do
 	python3 bgperf2.py update $i &
 done
