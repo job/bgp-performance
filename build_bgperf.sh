272ca4afcf53f62daccd7a632dc89d08f4c4fb69
Job Snijders <job@sobornost.net>
2024-12-30 22:09:15 +0000

    make containers one by one

--- build_bgperf.sh
+++ build_bgperf.sh
@@ -4,5 +4,5 @@ echo 16384 | sudo tee /proc/sys/net/ipv4/neigh/default/gc_thresh3
 sudo swapoff -a
 
 for i in openbgp87 openbgp86 bird bird3 gobgp; do
-	python3 bgperf2.py update $i &
+	python3 bgperf2.py update $i
 done
