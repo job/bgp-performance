69a22eba72183b928de169c2cda6328e0b83f001
Job Snijders <job@sobornost.net>
2025-03-04 15:05:58 +0000

    reshuffle test matrix

--- suite.sh
+++ suite.sh
@@ -5,7 +5,7 @@ set -ev
 ./bgperf2.py update gobgp
 ./bgperf2.py update bird
 
-for i in 86 85 84 83 82 81 80 79 78 77 76 75 74 73 72 71; do
+for i in 88 87 86; do
         ./bgperf2.py update openbgp$i &
 done
 
