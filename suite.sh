a6dfa94a2f2942e718c49b839678731acd5d7fc7
Job Snijders <job@sobornost.net>
2025-12-10 12:26:55 +0000

    keep building in foreground

--- suite.sh
+++ suite.sh
@@ -7,7 +7,7 @@ set -ev
 ./bgperf2.py update bird3
 
 for i in dev 89 88 87; do
-        ./bgperf2.py update openbgp$i &
+        ./bgperf2.py update openbgp$i
 done
 
 ./bgperf2.py batch -c big-tests.yaml
